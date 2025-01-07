3bd4eccc874bdd8019e0ce6505c4363ce2ea6ed4
abdulladone94 <abdulladone94@gmail.com>
2025-01-07 08:11:36 +0530

    connected to postgres db with node server

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+// Error Handling
+
+// Testing POSTGRES Connection
+app.get("/", async (req, res) => {
+  console.log("start");
+  const result = await pool.query("SELECT current_database()");
+  console.log("end");
+  res.send(`The database name is: ${result.rows[0].current_database}`);
+});
+
+// Server running
+app.listen(port, () => {
+  console.log(`Server is running on http:localhost:${port}`);
+});
