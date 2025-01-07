95b9b44a6ba9388d72771719f7f6a56a7f255bdf
abdulladone94 <abdulladone94@gmail.com>
2025-01-07 11:20:35 +0530

    implemented CRUD

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import pool from "./config/db.js";
 
+import userRoutes from "./routes/userRoutes.js";
+import errorHandling from "./middlewares/errorHandler.js";
+import createUserTable from "./data/createUserTable.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,8 +18,13 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.use("/api", userRoutes);
 
 // Error Handling
+app.use(errorHandling);
+
+// Create table before starting server
+createUserTable();
 
 // Testing POSTGRES Connection
 app.get("/", async (req, res) => {
